090eb49b336156346e0f8617629935c7d40f5708
Kaman Chamling <chamlingkaman@gmail.com>
2024-05-01 08:58:20 +0200

    minor ui change

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const count = document.querySelector(".count")
-const plusBtn = document.querySelector("#plusBtn")
-const minusBtn = document.querySelector("#minusBtn")
-const resetBtn = document.querySelector("#resetBtn")
-const changeBy = document.querySelector("#changeBy")
+const container = document.querySelector("#container");
+const count = document.querySelector(".count-container #count");
+count.innerText = 0;
+const plusBtn = document.querySelector(".button-controls #plus-btn");
+const minusBtn = document.querySelector(".button-controls #minus-btn");
+const changeBy = document.querySelector(".change-by input");
+const resetBtn = document.querySelector("#reset-btn");
 
 plusBtn.addEventListener("click", () => {
-  const countValue = parseInt(count.innerText)
-  const changeByValue = parseInt(changeBy.value)
-  count.innerText = countValue + changeByValue
-})
+  const currentCount = parseInt(count.innerText);
 
+  const changeByValue = parseInt(changeBy.value);
+
+  count.innerText = currentCount + changeByValue;
+});
 minusBtn.addEventListener("click", () => {
-  const countValue = parseInt(count.innerText)
-  const changeByValue = parseInt(changeBy.value)
-  count.innerText = countValue - changeByValue
-})
+  const currentCount = parseInt(count.innerText);
+  const changeByValue = parseInt(changeBy.value);
+  count.innerText = currentCount - changeByValue;
+});
 
 resetBtn.addEventListener("click", () => {
-  count.innerText = "0"
-})
+  count.innerText = 0;
+  changeByValue.value = "";
+});
