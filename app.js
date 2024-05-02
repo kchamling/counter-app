c51dd7be5294e46140c0b135043d4bdca3bc2145
Kaman Chamling <chamlingkaman@gmail.com>
2024-05-02 15:39:58 +0200

    minor bug fixed

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const container = document.querySelector("#container");
 const count = document.querySelector(".count-container #count");
-count.innerText = 0;
 const plusBtn = document.querySelector(".button-controls #plus-btn");
 const minusBtn = document.querySelector(".button-controls #minus-btn");
 const changeBy = document.querySelector(".change-by input");
 const resetBtn = document.querySelector("#reset-btn");
 
+count.innerText = 0;
+
 plusBtn.addEventListener("click", () => {
   const currentCount = parseInt(count.innerText);
-
+  console.log(currentCount);
   const changeByValue = parseInt(changeBy.value);
-
+  console.log(changeByValue);
   count.innerText = currentCount + changeByValue;
 });
+
 minusBtn.addEventListener("click", () => {
   const currentCount = parseInt(count.innerText);
   const changeByValue = parseInt(changeBy.value);
